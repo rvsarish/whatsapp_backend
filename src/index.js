726df72a542c72b9a39f0cff63ed9954714ed86d
rvsarish <21it210@stjosephs.ac.in>
2024-05-20 18:39:01 +0530

    logger of error

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ dotenv.config();
 
 
 const PORT = process.env.PORT || 8000;
+let server;
 
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
-    
+    throw new Error('This is an error');
 });
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    if(server) {
+        logger.info(`Server closed`);
+            process.exit(1);
+    } else {
+        process.exit(1);
+    }
+};
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
