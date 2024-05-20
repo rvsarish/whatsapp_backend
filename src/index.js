364f95d5bfc7d1ea787f3adb2cc1b8e21ea32380
rvsarish <21it210@stjosephs.ac.in>
2024-05-20 21:54:24 +0530

    final_backend

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import mongoose from 'mongoose';
 import app from './app.js';
 import logger from './configs/logger.config.js';
 
-
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 const DATABASE_URL = process.env.DATABASE_URL;
+if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
 // , {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 // }
-mongoose.connect(DATABASE_URL).then(() => {
-    logger.info('Connected to MongoDB');
-}).catch((error) => {
-    logger.error(error.message);
-});
-
-const PORT = process.env.PORT || 8000;
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to Mongodb.");
+  });
+  mongoose.connection.on("error", (err) => {
+    logger.error(`Mongodb connection error : ${err}`);
+    process.exit(1);
+  });
 let server;
 
 server = app.listen(PORT, () => {
