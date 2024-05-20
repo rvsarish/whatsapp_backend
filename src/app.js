c3c23f791c04e1c4395af93b20ecaf440fe1c17d
rvsarish <21it210@stjosephs.ac.in>
2024-05-20 17:45:31 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import fileUpload from 'express-fileupload';
 
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true}));
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+
+app.get('/test', (req, res) => {
     res.send('Hello World');
 });
 
